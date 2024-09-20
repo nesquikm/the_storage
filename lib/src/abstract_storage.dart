3775b7f346ebbbaadec09a31acef4afba688d2ab
nesquikm <drapolyuk@gmail.com>
2024-09-20 15:21:06 +0400

    docs: storage methods for better readability and documentation

--- lib/src/abstract_storage.dart
+++ lib/src/abstract_storage.dart
@@ -41,31 +41,100 @@ abstract class AbstractStorage<StorageValueType> {
     bool overwrite = true,
   });
 
-  /// Delete by [key] from [domain].
+  /// Delete the value associated with the given [key] from the specified
+  /// [domain].
+  ///
+  /// Example:
+  /// ```dart
+  /// await storage.delete('myKey', domain: 'myDomain');
+  /// ```
+  ///
+  /// Parameters:
+  /// - `key`: The key to delete.
+  /// - `domain`: The domain to delete the key from. Defaults to
+  ///   [defaultDomain].
+  ///
+  /// Returns:
+  /// - A [Future] that completes when the key is deleted.
   Future<void> delete(
     String key, {
     String domain = defaultDomain,
   });
 
-  /// Delete by [keys] from [domain].
+  /// Delete values associated with the given [keys] from the specified
+  /// [domain].
+  ///
+  /// Example:
+  /// ```dart
+  /// await storage.deleteDomain(['key1', 'key2'], domain: 'myDomain');
+  /// ```
+  ///
+  /// Parameters:
+  /// - `keys`: A list of keys to delete.
+  /// - `domain`: The domain from which to delete the keys. Defaults to
+  ///   [defaultDomain].
+  ///
+  /// Returns:
+  /// - A [Future] that completes when the keys are deleted.
   Future<void> deleteDomain(
     List<String> keys, {
     String domain = defaultDomain,
   });
 
-  /// Get value by [key] and [domain]. If not found will return [defaultValue]
+  /// Get the value associated with the given [key] from the specified [domain].
+  /// If the key is not found, it will return [defaultValue].
+  ///
+  /// Example:
+  /// ```dart
+  /// final value = await storage.get('myKey', defaultValue: 'default');
+  /// ```
+  ///
+  /// Parameters:
+  /// - `key`: The key to retrieve the value for.
+  /// - `defaultValue`: The value to return if the key is not found. Defaults to
+  ///   `null`.
+  /// - `domain`: The domain to retrieve the value from. Defaults to
+  ///   [defaultDomain].
+  ///
+  /// Returns:
+  /// - A [Future] that completes with the value associated with the key, or
+  ///   [defaultValue] if the key is not found.
   Future<StorageValueType?> get(
     String key, {
     StorageValueType? defaultValue,
     String domain = defaultDomain,
   });
 
-  /// Get key-value pairs map from [domain].
+  /// Get a map of key-value pairs from the specified [domain].
+  ///
+  /// Example:
+  /// ```dart
+  /// final map = await storage.getDomain(domain: 'myDomain');
+  /// ```
+  ///
+  /// Parameters:
+  /// - `domain`: The domain to retrieve the key-value pairs from. Defaults to
+  ///   [defaultDomain].
+  ///
+  /// Returns:
+  /// - A [Future] that completes with a map of key-value pairs from the domain.
   Future<Map<String, StorageValueType>> getDomain({
     String domain = defaultDomain,
   });
 
-  /// Get keys from [domain]
+  /// Get a list of keys from the specified [domain].
+  ///
+  /// Example:
+  /// ```dart
+  /// final keys = await storage.getDomainKeys(domain: 'myDomain');
+  /// ```
+  ///
+  /// Parameters:
+  /// - `domain`: The domain to retrieve the keys from. Defaults to
+  ///   [defaultDomain].
+  ///
+  /// Returns:
+  /// - A [Future] that completes with a list of keys from the domain.
   Future<List<String>> getDomainKeys({
     String domain = defaultDomain,
   });
