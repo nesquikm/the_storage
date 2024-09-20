44c655c770de7d45e9dffc1f404603b543638671
nesquikm <drapolyuk@gmail.com>
2024-09-20 15:43:56 +0400

    docs: add cool docs (#16)
    * docs: storage methods for better readability and documentation
    
    * docs: improve documentation and add reactive streams
    
    * docs: improve storage methods and documentation
    
    * fix: typo
    
    * chore: update dependencies

--- lib/src/abstract_storage.dart
+++ lib/src/abstract_storage.dart
@@ -8,20 +8,75 @@ abstract class AbstractStorage<StorageValueType> {
   /// Default storage file name
   static const storageFileName = 'storage.db';
 
-  /// Reset storage
+  /// Reset the entire storage.
+  ///
+  /// This method clears all data from the storage, effectively resetting it
+  /// to its initial state.
+  ///
+  /// Example:
+  /// ```dart
+  /// await storage.reset();
+  /// ```
+  ///
+  /// Returns:
+  /// - A [Future] that completes when the storage is reset.
   Future<void> reset();
 
-  /// Clear storage: all records
+  /// Clear all records from the storage.
+  ///
+  /// This method removes all key-value pairs from the storage.
+  ///
+  /// Example:
+  /// ```dart
+  /// await storage.clearAll();
+  /// ```
+  ///
+  /// Returns:
+  /// - A [Future] that completes when all records are cleared.
   Future<void> clearAll();
 
-  /// Clear storage: all records in one domain
+  /// Clear all records from a specific domain.
+  ///
+  /// This method removes all key-value pairs from the specified [domain].
+  ///
+  /// Example:
+  /// ```dart
+  /// await storage.clearDomain('myDomain');
+  /// ```
+  ///
+  /// Parameters:
+  /// - `domain`: The domain to clear. Defaults to [defaultDomain].
+  ///
+  /// Returns:
+  /// - A [Future] that completes when the domain is cleared.
   Future<void> clearDomain([String domain = defaultDomain]);
 
-  /// Write the key-value pair.
+  /// Write a key-value pair to the storage.
   ///
-  /// [value] will be written for the [key] in [domain].
-  /// If the pair was already existed it will be overwritten if [overwrite]
-  /// is true (by default)
+  /// This method writes the [value] for the given [key] in the specified
+  /// [domain]. If the pair already exists, it will be overwritten if
+  /// [overwrite] is true (default).
+  ///
+  /// Example:
+  /// ```dart
+  /// await storage.set(
+  ///   'myKey',
+  ///   'myValue',
+  ///   domain: 'myDomain',
+  ///   overwrite: true,
+  /// );
+  /// ```
+  ///
+  /// Parameters:
+  /// - `key`: The key to associate with the value.
+  /// - `value`: The value to store.
+  /// - `domain`: The domain to store the key-value pair in. Defaults to
+  ///   [defaultDomain].
+  /// - `overwrite`: Whether to overwrite the value if the key already exists.
+  ///   Defaults to `true`.
+  ///
+  /// Returns:
+  /// - A [Future] that completes when the value is set.
   Future<void> set(
     String key,
     StorageValueType value, {
@@ -29,43 +84,130 @@ abstract class AbstractStorage<StorageValueType> {
     bool overwrite = true,
   });
 
-  /// Write the key-value pair map.
+  /// Write a map of key-value pairs to the storage.
   ///
-  /// [pairs] will be written in [domain].
-  /// If the pair was already existed it will be overwritten if [overwrite]
-  /// is true (by default). Unspecified in [pairs] in db will not be altered
-  /// or deleted.
+  /// This method writes the [pairs] in the specified [domain]. If a pair
+  /// already exists, it will be overwritten if [overwrite] is true (default).
+  /// Unspecified pairs in the database will not be altered or deleted.
+  ///
+  /// Example:
+  /// ```dart
+  /// await storage.setDomain(
+  ///   {'key1': 'value1', 'key2': 'value2'},
+  ///   domain: 'myDomain',
+  ///   overwrite: true,
+  /// );
+  /// ```
+  ///
+  /// Parameters:
+  /// - `pairs`: A map of key-value pairs to store.
+  /// - `domain`: The domain to store the key-value pairs in. Defaults to
+  ///   [defaultDomain].
+  /// - `overwrite`: Whether to overwrite the values if the keys already exist.
+  ///   Defaults to `true`.
+  ///
+  /// Returns:
+  /// - A [Future] that completes when the key-value pairs are set.
   Future<void> setDomain(
     Map<String, StorageValueType> pairs, {
     String domain = defaultDomain,
     bool overwrite = true,
   });
 
-  /// Delete by [key] from [domain].
+  /// Delete the value associated with the given [key] from the specified
+  /// [domain].
+  ///
+  /// Example:
+  /// ```dart
+  /// await storage.delete('myKey', domain: 'myDomain');
+  /// ```
+  ///
+  /// Parameters:
+  /// - `key`: The key to delete.
+  /// - `domain`: The domain to delete the key from. Defaults to
+  ///   [defaultDomain].
+  ///
+  /// Returns:
+  /// - A [Future] that completes when the key is deleted.
   Future<void> delete(
     String key, {
     String domain = defaultDomain,
   });
 
-  /// Delete by [keys] from [domain].
+  /// Delete values associated with the given [keys] from the specified
+  /// [domain].
+  ///
+  /// Example:
+  /// ```dart
+  /// await storage.deleteDomain(['key1', 'key2'], domain: 'myDomain');
+  /// ```
+  ///
+  /// Parameters:
+  /// - `keys`: A list of keys to delete.
+  /// - `domain`: The domain from which to delete the keys. Defaults to
+  ///   [defaultDomain].
+  ///
+  /// Returns:
+  /// - A [Future] that completes when the keys are deleted.
   Future<void> deleteDomain(
     List<String> keys, {
     String domain = defaultDomain,
   });
 
-  /// Get value by [key] and [domain]. If not found will return [defaultValue]
+  /// Get the value associated with the given [key] from the specified [domain].
+  /// If the key is not found, it will return [defaultValue].
+  ///
+  /// Example:
+  /// ```dart
+  /// final value = await storage.get('myKey', defaultValue: 'default');
+  /// ```
+  ///
+  /// Parameters:
+  /// - `key`: The key to retrieve the value for.
+  /// - `defaultValue`: The value to return if the key is not found. Defaults to
+  ///   `null`.
+  /// - `domain`: The domain to retrieve the value from. Defaults to
+  ///   [defaultDomain].
+  ///
+  /// Returns:
+  /// - A [Future] that completes with the value associated with the key, or
+  ///   [defaultValue] if the key is not found.
   Future<StorageValueType?> get(
     String key, {
     StorageValueType? defaultValue,
     String domain = defaultDomain,
   });
 
-  /// Get key-value pairs map from [domain].
+  /// Get a map of key-value pairs from the specified [domain].
+  ///
+  /// Example:
+  /// ```dart
+  /// final map = await storage.getDomain(domain: 'myDomain');
+  /// ```
+  ///
+  /// Parameters:
+  /// - `domain`: The domain to retrieve the key-value pairs from. Defaults to
+  ///   [defaultDomain].
+  ///
+  /// Returns:
+  /// - A [Future] that completes with a map of key-value pairs from the domain.
   Future<Map<String, StorageValueType>> getDomain({
     String domain = defaultDomain,
   });
 
-  /// Get keys from [domain]
+  /// Get a list of keys from the specified [domain].
+  ///
+  /// Example:
+  /// ```dart
+  /// final keys = await storage.getDomainKeys(domain: 'myDomain');
+  /// ```
+  ///
+  /// Parameters:
+  /// - `domain`: The domain to retrieve the keys from. Defaults to
+  ///   [defaultDomain].
+  ///
+  /// Returns:
+  /// - A [Future] that completes with a list of keys from the domain.
   Future<List<String>> getDomainKeys({
     String domain = defaultDomain,
   });
--- lib/src/reactive_storage.dart
+++ lib/src/reactive_storage.dart
@@ -1,13 +1,44 @@
 import 'package:rxdart/rxdart.dart';
-
 import 'package:the_storage/src/abstract_storage.dart';
 
-/// Reactive interface for the storage
+/// Reactive interface for the storage.
+///
+/// This interface provides reactive streams for accessing and observing
+/// storage values. It leverages [BehaviorSubject] from the `rxdart` package
+/// to emit the current value and any subsequent changes.
 abstract class ReactiveStorage<StorageValueType> {
-  /// Returns a [BehaviorSubject] that will emit the current value of the [key]
-  /// and [domain]. If not found will return [defaultValue]. [keepAlive] will
-  /// keep the subject alive after the last subscriber has unsubscribed, so the
-  /// data will stay in memory instead of being reacquired from the storage.
+  /// Subscribes to the value associated with the given [key] in the specified
+  /// [domain].
+  ///
+  /// This method returns a [ValueStream] that emits the current value of the
+  /// [key] and any subsequent changes. If the key is not found, it will emit
+  /// [defaultValue]. The [keepAlive] parameter determines whether the subject
+  /// should stay alive after the last subscriber has unsubscribed, keeping the
+  /// data in memory instead of reacquiring it from the storage.
+  ///
+  /// Example:
+  /// ```dart
+  /// final stream = await reactiveStorage.subscribe(
+  ///   'myKey',
+  ///   defaultValue: 'default',
+  /// );
+  /// stream.listen((value) {
+  ///   print('Current value: $value');
+  /// });
+  /// ```
+  ///
+  /// Parameters:
+  /// - `key`: The key to subscribe to.
+  /// - `defaultValue`: The value to emit if the key is not found. Defaults to
+  ///   `null`.
+  /// - `domain`: The domain to retrieve the value from. Defaults to
+  ///   [AbstractStorage.defaultDomain].
+  /// - `keepAlive`: Whether to keep the subject alive after the last subscriber
+  ///   has unsubscribed. Defaults to `true`.
+  ///
+  /// Returns:
+  /// - A [Future] that completes with a [ValueStream] emitting the current
+  ///   value and any subsequent changes.
   Future<ValueStream<StorageValueType?>> subscribe(
     String key, {
     StorageValueType? defaultValue,
@@ -15,19 +46,63 @@ abstract class ReactiveStorage<StorageValueType> {
     bool keepAlive = true,
   });
 
-  /// Returns a [BehaviorSubject] that will emit the key-value pairs map from
-  /// [domain]. [keepAlive] will keep the subject alive after the last
-  /// subscriber has unsubscribed, so the data will stay in memory instead of
-  /// being reacquired from the storage.
+  /// Subscribes to the key-value pairs map from the specified [domain].
+  ///
+  /// This method returns a [ValueStream] that emits the current key-value pairs
+  /// map from the [domain] and any subsequent changes. The [keepAlive]
+  /// parameter determines whether the subject should stay alive after the last
+  /// subscriber has unsubscribed, keeping the data in memory instead of
+  /// reacquiring it from the storage.
+  ///
+  /// Example:
+  /// ```dart
+  /// final stream = await reactiveStorage.subscribeDomain(domain: 'myDomain');
+  /// stream.listen((map) {
+  ///   print('Current key-value pairs: $map');
+  /// });
+  /// ```
+  ///
+  /// Parameters:
+  /// - `domain`: The domain to retrieve the key-value pairs from. Defaults to
+  ///   [AbstractStorage.defaultDomain].
+  /// - `keepAlive`: Whether to keep the subject alive after the last subscriber
+  ///   has unsubscribed. Defaults to `true`.
+  ///
+  /// Returns:
+  /// - A [Future] that completes with a [ValueStream] emitting the current
+  ///   key-value pairs map and any subsequent changes.
   Future<ValueStream<Map<String, StorageValueType>>> subscribeDomain({
     String domain = AbstractStorage.defaultDomain,
     bool keepAlive = true,
   });
 
-  /// Returns a [BehaviorSubject] that will emit the keys from [domain].
-  /// [keepAlive] will keep the subject alive after the last subscriber has
-  /// unsubscribed, so the data will stay in memory instead of being reacquired
-  /// from the storage.
+  /// Subscribes to the keys from the specified [domain].
+  ///
+  /// This method returns a [ValueStream] that emits the current keys from the
+  /// [domain] and any subsequent changes. The [keepAlive] parameter determines
+  /// whether the subject should stay alive after the last subscriber has
+  /// unsubscribed, keeping the data in memory instead of reacquiring it from
+  /// the storage.
+  ///
+  /// Example:
+  /// ```dart
+  /// final stream = await reactiveStorage.subscribeDomainKeys(
+  ///   domain: 'myDomain',
+  /// );
+  /// stream.listen((keys) {
+  ///   print('Current keys: $keys');
+  /// });
+  /// ```
+  ///
+  /// Parameters:
+  /// - `domain`: The domain to retrieve the keys from. Defaults to
+  ///   [AbstractStorage.defaultDomain].
+  /// - `keepAlive`: Whether to keep the subject alive after the last subscriber
+  ///   has unsubscribed. Defaults to `true`.
+  ///
+  /// Returns:
+  /// - A [Future] that completes with a [ValueStream] emitting the current keys
+  ///   and any subsequent changes.
   Future<ValueStream<List<String>>> subscribeDomainKeys({
     String domain = AbstractStorage.defaultDomain,
     bool keepAlive = true,
